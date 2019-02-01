7643800d95a6aefa2304e58ba19e95e97028ef2c
Christopher Bull <CaffeinatedAndroid@users.noreply.github.com>
2019-02-01 14:30:51 +0000

    Update MessageStreamTask.java
    Removed unnecessary printlns and removed URL in favour of the passed URL params

--- app/src/main/java/com/example/wquist/goshna/ApiResponse/MessageStreamTask.java
+++ app/src/main/java/com/example/wquist/goshna/ApiResponse/MessageStreamTask.java
@@ -22,12 +22,11 @@ public class MessageStreamTask extends AsyncTask<URL, MessageResponse, Void> {
     @Override
     protected Void doInBackground(URL... urls) {
         try {
-            URL url = new URL("http://scc-dh-testbed.lancs.ac.uk:5000/goshna/api/flights/messages/9/stream");
-            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-            System.out.println("SSE, http response: " + urlConnection.getResponseCode());
-            readStream(new BufferedInputStream(urlConnection.getInputStream()));
+            for (URL url : urls) {
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                readStream(new BufferedInputStream(urlConnection.getInputStream()));
+            }
         } catch (IOException e) {
-            System.out.println("SSE activity, Error on url openConnection: " + e.getMessage());
             e.printStackTrace();
         }
         return null;
@@ -42,8 +41,6 @@ public class MessageStreamTask extends AsyncTask<URL, MessageResponse, Void> {
                 if(line.startsWith("data:")) { // Only 'data:' part of SSE is currently used
                     // Example:
                     // data: {"messages":[{"body":"Test 4","flight_id":1,"id":4,"time":1548776086}]}
-                    System.out.println("ServerSentEvents, SSE event: " + line); // TEMP
-
                     try {
                         JSONObject json = new JSONObject(line.substring(line.indexOf(':') + 2).trim());
                         JSONArray jsonMessages =  json.getJSONArray("messages");
